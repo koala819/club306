f933882436c8484bea8be5143e6b9cbaa3786732
koala819 <x.genolhac@gmail.com>
2022-09-27 18:32:45 +0200

    add Presentation to Layout

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -1,13 +1,14 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import Adhesion from "./Adhesion";
-
+import Presentation from "./Presentation";
 
 const Layout = ({content} : LayoutProps) => {
 console.log(content)
     return (
         <div>
             <Header />
+            <Presentation title={content.presentationTitle} txt={content.presentationTxt} txtButton={content.presentationTxtBtn} />
             <Adhesion title={content.adhesionTitle} txtBtn={content.adhesionTxtBtn} />
 
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,15 @@ const Home: NextPage = () => {
     const content =
         {
             adhesionTitle : 'Rejoignez nous en 30 secondes',
-            adhesionTxtBtn: 'Adhérer'
+            adhesionTxtBtn: 'Adhérer',
+            presentationTitle: 'Le Premier et Unique Club en France dédié à la Peugeot 306',
+            presentationTxt: 'Le club a été crée suite à un besoin exprimé par de nombreuses personnes qui sont membres de forums ou de groupes.',
+            presentationTxtBtn: 'En savoir plus ...'
         }
   return (
     <div>
 
-      {/*<Presentation />
-      <Adhesion />
+      {/*
       <Outings />
       <Partners />*/}
       <Layout content={content} />
