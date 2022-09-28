a74281686186654b24012e3bb226a968eed4fc83
koala819 <x.genolhac@gmail.com>
2022-09-28 15:09:06 +0200

    fix erreor react/no-unescaped-entities

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -19,7 +19,7 @@ const Contact : NextPage = () => {
                     </div>
                     <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                         <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Contact</h2>
-                        <p className="leading-relaxed mb-5 text-gray-600">Une question sur adhésion, le suivi d'une commande ou par rapport à un évènement ? Contactez nous ^_^</p>
+                        <p className="leading-relaxed mb-5 text-gray-600">Une question sur adhésion, le suivi d&apos;une commande ou par rapport à un évènement ? Contactez nous ^_^</p>
                         <div className="relative mb-4">
                             <label htmlFor="name" className="leading-7 text-sm text-gray-600">Nom</label>
                             <input type="text" id="name" name="name"
@@ -38,7 +38,7 @@ const Contact : NextPage = () => {
                         <button
                             className="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Envoyer
                         </button>
-                        <p className="text-xs text-gray-500 mt-3">L'équipe s'organise au mieux afin d'apporter une réponse dans les 24 heures. Sportivement</p>
+                        <p className="text-xs text-gray-500 mt-3">L&apos;équipe s&apos;organise au mieux afin d&apos;apporter une réponse dans les 24 heures. Sportivement</p>
                     </div>
                 </div>
             </section>
