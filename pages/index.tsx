7d8b4f500f775f602f069ae22c23a3d3acb9f77e
koala819 <x.genolhac@gmail.com>
2022-09-28 15:18:36 +0200

    update index.tsx with remove Body & Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
-import type { NextPage } from 'next'
-import Layout from './components/Layout'
+import Adhesion from './components/Adhesion'
+import Outings from './components/Outings'
+import Partners from './components/Partners'
+import Footer from './components/Footer'
+import Header from "./components/Header";
+import Presentation from "./components/Presentation";
+import {NextPage} from "next";
 
 const Home: NextPage = () => {
+
     const content =
         {
             adhesionTitle : 'Rejoignez nous en 30 secondes',
@@ -10,15 +16,17 @@ const Home: NextPage = () => {
             presentationTxt: 'Le club a été crée suite à un besoin exprimé par de nombreuses personnes qui sont membres de forums ou de groupes.',
             presentationTxtBtn: 'En savoir plus ...'
         }
-  return (
-    <div>
 
-      {/*
-      <Outings />
-      <Partners />*/}
-      <Layout content={content} />
+    return (
+        <div>
+        <Header />
+        <Presentation title={content.presentationTitle} txt={content.presentationTxt} txtButton={content.presentationTxtBtn} />
+        <Adhesion title={content.adhesionTitle} txtBtn={content.adhesionTxtBtn} />
+        <Outings />
+        <Partners />
+        <Footer />
     </div>
-  )
-}
+    )
+    }
 
 export default Home
