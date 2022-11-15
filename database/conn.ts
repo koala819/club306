a2ba784620891f104fa9cdfcd2c54027725dd5b0
koala819 <x.genolhac@gmail.com>
2022-11-15 11:55:31 +0100

    install & configure mongo

---
+++ database/conn.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+export default async  function connectMongo() {
+    try {
+        // @ts-ignore
+        const {connection}: any = await mongoose.connect(process.env.MONGO_URL)
+
+        if (connection?.readyState === 1) {
+            return Promise.resolve(true)
+        }
+    } catch (error) {
+        return Promise.reject(error)
+    }
+}
