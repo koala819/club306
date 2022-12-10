391899f6bc8f8684a282897441006282797045aa
koala819 <x.genolhac@gmail.com>
2022-12-11 00:02:40 +0100

    change navbar color when user is connected

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,12 @@
 import Navbar from './Navbar';
 
-export default function header (){
+export default function Header (props: any){
+    console.log('Header props',props)
+
     return (
     <div className="fixed w-full  inset-x-0 top-0 z-50">
-        <Navbar />
-        {/*  */}
+        <Navbar bgColor={props} />
+
     </div>
     ) 
 }
