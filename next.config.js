785c05a309dc44163b2280164b386181a7755b4f
koala819 <x.genolhac@gmail.com>
2022-12-10 12:15:45 +0100

    fix env variable CLIENT_URL

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   env: {
     PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
-    PAYPAL_APP_SECRET: process.env.PAYPAL_APP_SECRET
+    PAYPAL_APP_SECRET: process.env.PAYPAL_APP_SECRET,
+    CLIENT_URL: process.env.CLIENT_URL
   }
 };
