59d2bb09bbbd0d393ceea85f6e6d40c43a008b94
koala819 <x.genolhac@gmail.com>
2022-12-13 13:10:17 +0100

    react-intersection-observer with Adhesion

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import Link from 'next/link';
 import { signOut, useSession } from 'next-auth/react';
 import { createClient } from '@supabase/supabase-js';
 import { useState } from 'react';
+import Adhesion from '../components/Adhesion';
 
 export default function Index() {
   const { data: session } = useSession();
@@ -134,35 +135,7 @@ function _Guest() {
 
       {/*Section 03*/}
       <section className='w-full h-5/6'>
-        <section className='text-gray-600 body-font text-white bg-blue-500 border-0 mt-4'>
-          <div className='container px-5 py-24 mx-auto flex items-center md:flex-row flex-col'>
-            <div className='flex flex-col md:pr-10 md:mb-0 mb-6 pr-0 w-full md:w-auto md:text-left text-center'>
-              <h1 className='md:text-3xl text-2xl font-medium title-font text-white'>
-                Rejoignez nous en 30 secondes
-              </h1>
-            </div>
-            <div className='flex md:ml-auto md:mr-0 mx-auto items-center flex-shrink-0 space-x-4'>
-              <button
-                className='bg-amber-500 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-200 focus:outline-none'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='currentColor'
-                  className='w-6 h-6'
-                  viewBox='0 0 512 512'
-                >
-                  <path
-                    d='M99.617 8.057a50.191 50.191 0 00-38.815-6.713l230.932 230.933 74.846-74.846L99.617 8.057zM32.139 20.116c-6.441 8.563-10.148 19.077-10.148 30.199v411.358c0 11.123 3.708 21.636 10.148 30.199l235.877-235.877L32.139 20.116zM464.261 212.087l-67.266-37.637-81.544 81.544 81.548 81.548 67.273-37.64c16.117-9.03 25.738-25.442 25.738-43.908s-9.621-34.877-25.749-43.907zM291.733 279.711L60.815 510.629c3.786.891 7.639 1.371 11.492 1.371a50.275 50.275 0 0027.31-8.07l266.965-149.372-74.849-74.847z'></path>
-                </svg>
-                <span className='ml-4 flex items-start flex-col leading-none '>
-                <span className='text-xs text-white mb-1'>
-                  <Link href='membership'>Adhérer</Link>
-                </span>
-              </span>
-              </button>
-            </div>
-          </div>
-        </section>
-
+       <Adhesion />
       </section>
 
       <section className='w-full h-screen'>
