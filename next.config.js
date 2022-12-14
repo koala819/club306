3b418d1418115b3e262a3d7960f3297ef1307ddc
koala819 <x.genolhac@gmail.com>
2022-12-14 10:48:24 +0100

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true
-};
-
-module.exports = {
-  nextConfig,
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-  },
-  env: {
-    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
-    PAYPAL_APP_SECRET: process.env.PAYPAL_APP_SECRET,
-    CLIENT_URL: process.env.CLIENT_URL
-  }
+module.exports = async () => {
+  return {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: ['lh3.googleusercontent.com']
+    },
+    env: {
+      PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
+      PAYPAL_APP_SECRET: process.env.PAYPAL_APP_SECRET,
+      CLIENT_URL: process.env.CLIENT_URL
+    }
+  };
 };
