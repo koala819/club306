74a4f2da71b4e1f4fb7f3a5ab3dab92f7a3d4d23
koala819 <x.genolhac@gmail.com>
2022-12-16 19:26:07 +0100

    CSS MembershipContent 4 finished

--- components/MembershipContent4.tsx
+++ components/MembershipContent4.tsx
@@ -1,5 +1,8 @@
 import { getSession } from 'next-auth/react';
-
+import { RxAvatar } from 'react-icons/rx'
+import { BsCaretRightFill } from 'react-icons/bs'
+import Image from 'next/image';
+import Link from 'next/link'
 import { useEffect, useState } from 'react';
 import ClipLoader from 'react-spinners/ClipLoader';
 
@@ -10,7 +13,7 @@ export default function MembershipContent4 (nextStep: any) {
   const [isRegistered, setIsRegistered] = useState(false);
   const [googleName, setGoogleName] = useState('');
   const [googleEmail, setGoogleEmail] = useState('');
-  /*const [googlePicture, setGooglePicture] = useState('');*/
+  const [googlePicture, setGooglePicture] = useState('');
 
 
   useEffect(() => {
@@ -24,7 +27,7 @@ export default function MembershipContent4 (nextStep: any) {
         if (session) {
           session.user?.name && setGoogleName(session.user.name);
           session.user?.email && setGoogleEmail(session.user.email);
-          /*session.user?.image && setGooglePicture(session.user.image);*/
+          session.user?.image && setGooglePicture(session.user.image);
         } else {
           console.log('Aucune identification avec un compte Google trouvé !');
         }
@@ -94,90 +97,61 @@ export default function MembershipContent4 (nextStep: any) {
               </div>
             )}
 
-          </section>
-        </div>
-        }
-      </main>
-      {/*<section className='min-h-screen flex items-stretch text-white pt-8'>
-
-        <div className='lg:flex w-1/2 hidden bg-gray-500 bg-no-repeat bg-cover relative items-center'
-             style={{ backgroundImage: `url(https://images.unsplash.com/photo-1568106575207-0fe3ec317559?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2662&q=80)` }}
-        >
-          <div className='absolute bg-black opacity-60 inset-0 z-0'></div>
-          <div className='w-full px-24 z-10'>
-            <h1 className='text-5xl font-bold text-left tracking-wide'>Club 306</h1>
-            <p className='text-3xl my-4'>Rejoins nous dans l&apos;aventure Peugeot 306</p>
-          </div>
-        </div>
-
-        <div className='lg:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0 bg-gray-700'>
-          <div className='flex border rounded-xl relative text-gray-600 body-font py-24'>
-
-            {!isRegistered && (
-              <div className='text-white flex flex-col text-center space-y-8 m-x-4 px-5'>
-                <ClipLoader
-                  className='mx-auto'
-                  color={'#FFF'}
-                  loading={true}
-                  size={50}
-                  aria-label='Loading Spinner'
-                  data-testid='loader'
-                />
-                <p>Enregistrement des informations dans notre base de données</p>
-                <p>Veuillez patienter</p>
-              </div>
-            )}
-
             {isRegistered && (
-              <div>
-                <h1 className='sm:text-3xl text-2xl font-medium title-font mb-4 text-white'>Merci</h1>
+              <div className=' center grid place-items-center'>
+                <h1 className='sm:text-3xl text-2xl font-medium title-font mb-4 '>Merci</h1>
                 {googlePicture
                   ? <Image
-                  alt='picture user'
-                  height={100}
-                  width={100}
-                  className='rounded-full'
-                  src={`${googlePicture}`}
-                  style={{
-                    maxWidth: "100%",
-                    height: "auto"
-                  }} />
+                    alt='picture user'
+                    height={100}
+                    width={100}
+                    className='rounded-full'
+                    src={`${googlePicture}`}
+                    style={{
+                      maxWidth: "100%",
+                      height: "auto"
+                    }} />
                   : <span className='flex justify-center '><RxAvatar size={100} /></span>
                 }
                 <div className='flex-grow'>
-                  <h2 className='text-white title-font font-medium'>
+                  <h2 className='title-font font-medium'>
                     {googleName || dataSession?.last_name + ' ' + dataSession?.first_name}
                   </h2>
-                  <p className='text-white ml-4'>
+                  <p className='ml-4'>
                     {googleEmail || dataSession?.email}
                   </p>
                 </div>
-                <p className='lg:w-2/3 mx-auto leading-relaxed text-base text-gray-50 mt-8'>pour ta
+                <p className='lg:w-2/3 mx-auto leading-relaxed text-base mt-8'>pour ta
                   participation à
                   cette
                   grande et belle aventure mécanique autour de la merveilleuse 306.</p>
                 {googleEmail ? <Link href='/'>
-                  <button
-                    className='bg-green-600 hover:bg-green-400 active:bg-green-800 text-black font-bold py-2 px-4 rounded inline-flex items-center mt-8'>
-                    <BsCaretRightFill size={23} />
-                    <span>Terminer</span>
-                  </button>
-                </Link>
-                : <Link href='login'>
-                    <button
-                      className='bg-green-600 hover:bg-green-400 active:bg-green-800 text-black font-bold py-2 px-4 rounded inline-flex items-center mt-8'>
-                      <BsCaretRightFill size={23} />
-                      <span>Terminer</span>
-                    </button>
+                    <div
+                      className='border rounded-md hover:bg-[#DB2323] bg-[#3B578E] flex justify-center items-center py-2 px-4 '>
+                      <button
+                        type='submit'
+                        className='text-[#F7F9FF] text-xl'>
+                        Terminer
+                      </button>
+                    </div>
+                  </Link>
+                  : <Link href='login'>
+                    <div
+                      className='border rounded-md hover:bg-[#DB2323] bg-[#3B578E] flex justify-center items-center py-2 px-4 '>
+                      <button
+                        type='submit'
+                        className='text-[#F7F9FF] text-xl'>
+                        Terminer
+                      </button>
+                    </div>
                   </Link>}
               </div>
             )}
-          </div>
 
-
+          </section>
         </div>
-      </section>*/}
-
+        }
+      </main>
     </div>
   );
 }
