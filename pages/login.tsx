8e735d59f211354e2cfdb831f420153248b7dff8
koala819 <x.genolhac@gmail.com>
2022-12-20 17:24:55 +0100

    fix CSS with laptop Pascal

--- pages/login.tsx
+++ pages/login.tsx
@@ -42,106 +42,118 @@ export default function Login() {
 
   return (
     <Layout title='Ecran Connexion'>
-      <main className='flex-1'>
-        {(height !== 0) && <div className='flex' style={{ height: `${height}px` }}>
+      <div className='container mx-auto'>
+        <main className='flex-1'>
+          {(height !== 0) && <div className='flex' style={{ height: `${height}px` }}>
 
-          {/*LEFT*/}
-          <section className='lg:flex w-1/2 hidden bg-cover'
-                   style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1568106575207-0fe3ec317559)' }}
-          >
-            {/*La hauteur de cette div est : {height} px*/}
-            <div className=' text-black flex items-center justify-center bg-white opacity-60 inset-0 z-0 h-full w-full'>
-              <div>
-                <h1 className='text-5xl tracking-wide'>Club 306</h1>
-                <p className='text-3xl my-4'>Rejoins nous dans l&apos;aventure Peugeot 306</p>
-              </div>
-            </div>
-          </section>
-
-          {/*RIGHT*/}
-          <section className='h-full lg:w-1/2 w-full lg:flex flex-col justify-between'>
-            <div>
-              <p className='text-xl font-[999] pt-6 ml-8'>
-                Connexion à votre compte
-              </p>
-              <div className='flex mb-6 ml-8 text-sm'>
-                <p className='text-gray-400 mr-2 '>
-                  Pas encore de compte ?
-                </p>
-                <p className='hover:underline hover:font-[999]'>
-                  <Link href='membership'>Créez en un</Link>
-                </p>
-              </div>
-            </div>
-
-            <div
-              className='border rounded-md border-black hover:border-blue-400 hover:bg-blue-50 flex justify-center items-center py-2 mb-4 mx-20'>
-              <button type='button'
-                      onClick={() => _handleGoogleSignin}
-                      className=''>
-
-              </button>
-              <span className='flex'>
-                <FcGoogle size={25} className='mr-2' />
-                  Continuer avec Google
-              </span>
-            </div>
-
-            <div className='relative flex py-5 items-center mx-4 mb-8'>
-              <div className='flex-grow border-t border-gray-400 '></div>
-              <span className='flex-shrink mx-4 '>OU</span>
-              <div className='flex-grow border-t border-gray-400'></div>
-            </div>
-
-            <form action=''
-                  className='sm:w-2/3 w-full px-4 lg:px-0 mx-auto mb-16'
-                  onSubmit={formik.handleSubmit}
+            {/*LEFT*/}
+            <section className='lg:flex w-1/2 hidden bg-cover'
+                     style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1568106575207-0fe3ec317559)' }}
             >
+              {/*La hauteur de cette div est : {height} px*/}
               <div
-                className={`${'flex border rounded-xl relative'} ${formik.errors.email && formik.touched.email ? 'border-rose-600' : ''}`}>
-                <input type='email'
-                       placeholder='Email'
-                       className='w-full py-4 px-6 border rounded-xl focus:outline-none border-none'
-                       {...formik.getFieldProps('email')}
-                />
-                <span className='icon flex items-center px-4 text-[#3B578E] '>
-                  <HiAtSymbol size={25} />
-                </span>
+                className=' text-black flex items-center justify-center bg-white opacity-60 inset-0 z-0 h-full w-full'>
+                <div>
+                  <h1 className='text-5xl tracking-wide'>Club 306</h1>
+                  <p className='text-3xl my-4'>Rejoins nous dans l&apos;aventure Peugeot 306</p>
+                </div>
+              </div>
+            </section>
+
+            {/*RIGHT*/}
+            <section className='w-full flex flex-col justify-between my-4'>
+
+              {/*Section HAUT*/}
+              <div className=''>
+                <p className='text-xl font-[999] pt-6 ml-8'>
+                  Connexion à votre compte
+                </p>
+                <div className='flex mb-6 ml-8 text-sm'>
+                  <p className='text-gray-400 mr-2 '>
+                    Pas encore de compte ?
+                  </p>
+                  <p className='hover:underline hover:font-[999]'>
+                    <Link href='membership'>Créez en un</Link>
+                  </p>
+                </div>
               </div>
 
+              {/*Section MILIEU*/}
+              <div className=''>
+                <div
+                  className='border rounded-md border-black hover:border-blue-400 hover:bg-blue-50 flex justify-center items-center py-2 mb-4 mx-56'>
+                  <button type='button'
+                          onClick={() => _handleGoogleSignin}
+                          className=''>
 
-              <div
-                className={`${'my-3 flex border rounded-xl relative '} ${formik.errors.password && formik.touched.password ? 'border-rose-600' : ''}`}>
-                <input type={`${show.password ? 'text' : 'password'}`}
-                       placeholder='Mot de Passe'
-                       className='w-full py-4 px-6 border rounded-xl focus:outline-none border-none'
-                       {...formik.getFieldProps('password')}
-                />
-                <span className='icon flex items-center px-4 text-[#3B578E] hover:text-[#DB2323] active:text-[#D7DEED]'
-                      onClick={() => setShow({ ...show, password: !show.password })}>
-                  <AiOutlineEye size={25} />
-                </span>
+                  </button>
+                  <span className='flex'>
+                    <FcGoogle size={25} className='mr-2' />
+                    Continuer avec Google
+                  </span>
+                </div>
+                <div className='relative flex py-5 items-center mx-4 mb-8'>
+                  <div className='flex-grow border-t border-gray-400 '></div>
+                  <span className='flex-shrink mx-4 '>OU</span>
+                  <div className='flex-grow border-t border-gray-400'></div>
+                </div>
               </div>
 
+              {/*Section BAS*/}
+              <div className='flex justify-center items-center -mt-16'>
+                <form action=''
+                      className=''
+                      onSubmit={formik.handleSubmit}
+                >
+                  <div
+                    className={`${'flex border rounded-xl'} ${formik.errors.email && formik.touched.email ? 'border-rose-600' : ''}`}>
+                    <input type='email'
+                           placeholder='Email'
+                           className='border rounded-xl focus:outline-none border-none'
+                           {...formik.getFieldProps('email')}
+                    />
+                    <span className='flex items-center px-2 flex text-[#3B578E] '>
+                      <HiAtSymbol size={25} />
+                    </span>
+                  </div>
 
-              {/*<div className='text-right text-gray-400 hover:underline hover:text-gray-100'>
-                <Link href='findPwd'>Mot de passe oublié ?</Link>
-              </div>*/}
-              <div
-                className='mt-12 border rounded-md hover:bg-[#DB2323] bg-[#3B578E] flex justify-center items-center py-2 mb-4 mx-20'>
-                <button
-                  type='submit'
-                  className='text-[#F7F9FF] text-xl'>
-                  Se Connecter
-                </button>
+                  <div
+                    className={`${'my-3 flex border rounded-xl'} ${formik.errors.password && formik.touched.password ? 'border-rose-600' : ''}`}>
+                    <input type={`${show.password ? 'text' : 'password'}`}
+                           placeholder='Mot de Passe'
+                           className='border rounded-xl focus:outline-none border-none'
+                           {...formik.getFieldProps('password')}
+                    />
+                    <span className='flex items-center px-2 text-[#3B578E] hover:text-[#DB2323] active:text-[#D7DEED]'
+                          onClick={() => setShow({ ...show, password: !show.password })}>
+                      <AiOutlineEye size={25} />
+                    </span>
+                  </div>
+
+                  <div className='text-right text-gray-400 hover:underline hover:text-gray-100'>
+                    <Link href='findPwd'>Mot de passe oublié ?</Link>
+                  </div>
+                  <div
+                    className='mt-4 border rounded-md hover:bg-[#DB2323] bg-[#3B578E] flex justify-center items-center py-2 px-2'>
+                    <button
+                      type='submit'
+                      className='text-[#F7F9FF] text-xl'>
+                      Se Connecter
+                    </button>
+                  </div>
+                </form>
               </div>
 
-            </form>
-          </section>
-        </div>
-        }
-      </main>
+            </section>
+
+
+          </div>
+          }
+        </main>
+      </div>
+
     </Layout>
+
   );
 }
 
