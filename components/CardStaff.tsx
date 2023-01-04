aa46efb4ad64f82772bcf2ca8fab6b00c0a920d2
koala819 <x.genolhac@gmail.com>
2023-01-04 23:42:31 +0100

    update staff page

--- components/CardStaff.tsx
+++ components/CardStaff.tsx
@@ -1,92 +1,100 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 
 
-const CardStaff = ({picture, name, role, content, car, localisation, facebook, insta, mail}: CardStaffProps) => {
-    return (
-        <div>
+export default function CardStaff({
+                                    picture,
+                                    name,
+                                    role,
+                                    content,
+                                    car,
+                                    localisation,
+                                    facebook,
+                                    insta,
+                                    mail
+                                  }: CardStaffProps) {
+  return (
+    <section className=' rounded-lg mb-8 lg:bg-white bg-red-500 overflow-y-auto sm:bg-blue-300'>
+      <div className='center grid place-items-center'>
 
-                        <div className="mb-24 md:mb-0">
-                            <div className="rounded-lg shadow-lg h-full block bg-white">
-                                <div className="flex justify-center">
-
-                                    <div className="pt-2 w-1/2 relative " style={{height: '12rem'}}>
-                                        <Image
-                                            src={picture}
-                                            alt="photo membre club306"
-                                            className="rounded-full mx-auto shadow-lg"
-                                            fill
-                                            sizes="100vw" />
-                                    </div>
-                                </div>
-                                <div className="p-6">
-                                    <h5 className="text-lg font-bold mb-4">{name}</h5>
-                                    <p className="text-gray-500 mb-6">{role}</p>
-                                    <p className="mb-4">{content}</p>
-                                    <p className="mb-4">Voiture(s): {car}</p>
-                                    <p className="mb-4">Localisation: {localisation}</p>
-                                    <ul className="list-inside flex mx-auto justify-center">
-                                        <Link
-                                            href={facebook}
-                                            className="text-gray-500">
-                                            <svg
-                                                fill="currentColor"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                className="w-5 h-5"
-                                                viewBox="0 0 24 24"
-                                            >
-                                                <path
-                                                    d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                                            </svg>
-                                        </Link>
-                                        <Link
-                                            href={insta}
-                                            className="ml-3 text-gray-500">
-                                            <svg
-                                                fill="none"
-                                                stroke="currentColor"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                className="w-5 h-5"
-                                                viewBox="0 0 24 24"
-                                            >
-                                                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                                                <path
-                                                    d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                                            </svg>
-                                        </Link>
-                                        <Link
-                                            href={mail}
-                                            className="ml-4 text-gray-500">
-                                            <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round"
-                                                 strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                                <path
-                                                    d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                                            </svg>
-                                        </Link>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
-
+        <div className='w-full  flex justify-center'>
+          <div className="text-center relative" >
+            <Image
+              src={picture}
+              className="rounded-full w-32 mb-4 mx-auto mt-2"
+              alt="photo membre club306"
+              width="100"
+              height="100"
+            />
+            <h5 className="text-xl font-medium leading-tight mb-2">{name}</h5>
+            <p className="text-gray-500">{role}</p>
+          </div>
         </div>
-    );
+
+        <div className='mx-2 mt-4'>{content}</div>
+
+        <div>
+          <p className='my-4'>Voiture(s): {car}</p>
+          <p>Localisation: {localisation}</p>
+          <ul className='list-inside flex mx-auto justify-center my-4'>
+            <Link
+              href={facebook}
+              className='text-gray-500'>
+              <svg
+                fill='currentColor'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                className='w-5 h-5'
+                viewBox='0 0 24 24'
+              >
+                <path
+                  d='M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z'></path>
+              </svg>
+            </Link>
+            <Link
+              href={insta}
+              className='ml-3 text-gray-500'>
+              <svg
+                fill='none'
+                stroke='currentColor'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                className='w-5 h-5'
+                viewBox='0 0 24 24'
+              >
+                <rect width='20' height='20' x='2' y='2' rx='5' ry='5'></rect>
+                <path
+                  d='M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01'></path>
+              </svg>
+            </Link>
+            <Link
+              href={mail}
+              className='ml-4 text-gray-500'>
+              <svg fill='currentColor' strokeLinecap='round' strokeLinejoin='round'
+                   strokeWidth='2' className='w-5 h-5' viewBox='0 0 24 24'>
+                <path
+                  d='M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z'></path>
+              </svg>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
+
 export interface CardStaffProps {
-    picture: string
-    name?: string,
-    role?: string,
-    content?: string,
-    car?: string,
-    localisation?: string,
-    facebook?: any,
-    insta?: any,
-    mail?: any,
-    children?: React.ReactNode
+  picture: string
+  name?: string,
+  role?: string,
+  content?: string,
+  car?: string,
+  localisation?: string,
+  facebook?: any,
+  insta?: any,
+  mail?: any,
+  children?: React.ReactNode
 }
-export default CardStaff
--- pages/staff.tsx
+++ pages/staff.tsx
@@ -6,162 +6,126 @@ import CardStaff from "../components/CardStaff";
 export default function Staff () {
     const StaffMember = [
         {
-            picture: "/images/clubGaucherotFabrice.jpg",
-            name: "Fabrice Gaucherot",
+            picture: "/images/clubBesnardPascal.jpg",
+            name: "Pascal Besnard",
             role: "Président",
-            content: "J’ai appris à conduire avec une 306 1.6l 90ch ph 1 bleu miami\n" +
-                " importée d’Italie. Une fois le permis en poche plusieurs Peugeot sont passées\n" +
-                " (104 gls,106 xsi, 205 xt) jusqu’à ma 306 1.4l 75ch blanc banquise série limitée\n" +
-                "  Palm Beach ph 2.1. La famille s’est agrandie et d’autres voitures sont passées\n" +
-                " (205 gti 1.6) jusqu’à ma 306 2.0l S16 gris quartz ph 2.1 que je m’efforce de\n" +
-                " restaurer au fil du temps.",
-            car: "306 2.0 BV6 S16 ph 2.1 de 1997",
-            localisation: "Clermont-Ferrand (63)",
+            content: "Passionné de 306 depuis son apparition, j’ai suivi avec entousiasme son evolution et impact en\n" +
+              " France (sur les routes et rallye). En 2004, j’ai acquis une 306 1.9 Td bleu sigma ph 2.1. Puis en 2010,\n" +
+              " je l ai vendu pour m’acheter une 306 HDI que j’ai preparé pendant 3 ans. Enfin, je me suis acheté\n" +
+              " la version XU10J4RS que j’ai restauré à neuf.",
+            car: "306 S16 BV6 1997 Blanc Banquise Premium et 306 S16 BV6 1999 Gris Iceland",
+            localisation: "Île-de-France (93), Angers (49), Toulouse (31)",
             facebook: "https://www.facebook.com/club306france/",
             insta: "https://www.instagram.com/club306officielfrance",
             mail: "president@club306.fr"
         },
         {
-            picture: "/images/clubBienaszAurelien.jpg",
-            name: "Aurélien Bienasz",
+            picture: "/images/clubGaucherotFabrice.jpg",
+            name: "Fabrice Gaucherot",
             role: "Trésorier",
-            content: "En 2006 étant encore étudiant j’ai commencé par une 2.0 hdi de 2001 qui me\n" +
-                " faisait envie depuis des années. Puis après un accident j’ai décidé de passer à autre\n" +
-                "  chose. En 2012 je me suis mis en quête d’une sportive type 205 gti mais je suis tombé par\n" +
-                "  hasard sur une 306 s16 acav (le virus avait repris le dessus) que je possède toujours et\n" +
-                "  qui est en restauration petit à petit. Depuis 2018 la 1.4 l’a rejointe comme voiture de tous\n" +
-                "  les jours et pour les travaux de la maison.",
-            car: "306 1.4 Profil ph.1 de 97 et 306 s16 acav de 94",
-            localisation: "Pinterville (27)",
+            content: "J’ai appris à conduire avec une 306 1.6l 90ch ph 1 bleu miami\n" +
+              " importée d’Italie. Une fois le permis en poche plusieurs Peugeot sont passées\n" +
+              " (104 gls,106 xsi, 205 xt) jusqu’à ma 306 1.4l 75ch blanc banquise série limitée\n" +
+              "  Palm Beach ph 2.1. La famille s’est agrandie et d’autres voitures sont passées\n" +
+              " (205 gti 1.6) jusqu’à ma 306 2.0l S16 gris quartz ph 2.1 que je m’efforce de\n" +
+              " restaurer au fil du temps.",
+            car: "Ex 306 2.0 BV6 S16",
+            localisation: "Clermont-Ferrand (63)",
             facebook: "https://www.facebook.com/club306france/",
             insta: "https://www.instagram.com/club306officielfrance",
             mail: "tresorier@club306.fr"
         },
         {
-            picture: "/images/clubBerthetYoann.jpg",
-            name: "Yoann Berthet",
-            role: "Secrétaire/Vice-President",
-            content: "Commencée en 2003 lors de la recherche de ma première voiture. Je suis tombé vite amoureux de ma\n" +
-                " 306 1.4. Petit moteur mais qui m’as permis de faire des rassemblements, apprendre la mécanique et\n" +
-                " surtout rencontrer, beaucoup de personnes qui aujourd’hui ne sont pas que de simples amis passionnés.\n" +
-                " Cette passion m’a amené à acquérir une 1.9TD eden park de 95 qui malheureusement était trop malade\n" +
-                " pour être sauvée. Remplacée très vite par le Break qui lui subit une remise en forme. Présent dès\n" +
-                " le début de l’aventure du Club, pour essayer de réunir un maximum de monde sous une même bannière,\n" +
-                " car la passion c’est autour de toutes les 306 modèles et motorisations confondues",
-            car: "306 1.4L ph2. 2 de 1999 et 306 Break 2L Hdi de 2001",
-            localisation: "Lorient (56)",
-            facebook: "https://www.facebook.com/club306france/",
-            insta: "https://www.instagram.com/club306officielfrance",
-            mail: "secretariat@club306.fr"
-        },
-        {
-            picture: "/images/clubCheminSebastien.jpg",
-            name: "Sébastien Chemin",
-            role: "Responsable boutique",
-            content: "À la base, grand fan de Peugeot de père en fils donc la peugeot 306 1.8 16s a été ma première\n" +
-                " voiture avec qui j’ai eu beaucoup de bons et mauvais souvenirs. Entre temps j’ai eu différents modèles\n" +
-                " de la marque aux Lions (204 break, 207, 407, et puis le retour aux sources 306 GTI, HDI et XSI)",
-            car: "306 GTI (origine Suisse) de 2000 et 306 XSI 8s (origine Allemande) de 1995",
-            localisation: "Marne-la-Vallée (77)",
-            facebook: "https://www.facebook.com/club306france/",
-            insta: "https://www.instagram.com/club306officielfrance",
-            mail: "boutique@club306.fr"
-        },
-        {
-            picture: "/images/clubAzaisAntony.png",
-            name: "Antony Azais",
-            role: "Adjoint boutique",
-            content: "Mon premier achat, mon propre cadeau d’anniversaire, a été une voiture pas trop chère une 306\n" +
-                " 1.9d ph1.1 et très économique pour un jeune de 17 ans. J’ai aujourd’hui bientôt 26 ans et j’ai\n" +
-                " actuellement une 306 HDI comme daily et une 306 S16 bv6 ph1 pour les moments de plaisir et de détente.\n" +
-                " Ce qui me plaît ce sont ses formes carré des années 90 qui font penser à ces petites sœurs comme la 106\n" +
-                " XSI ou rallye ou même encore la 205, la « simplicité » et l’accessibilité aux réparations pour un peux\n" +
-                " expérimenté dans le domaine de la mécanique comme moi.",
-            car: "306 2.0 HDI 5p de 99 et 306 s16 BV6 de 97",
-            localisation: "Brive-la-Gaillarde (19)",
-            facebook: "https://www.facebook.com/club306france/",
-            insta: "https://www.instagram.com/club306officielfrance",
-            mail: "secretariat@club306.fr"
-        },
-        {
-            picture: "/images/clubFaurieBastien.jpg",
-            name: "Bastien Faurie",
-            role: "Responsable Partenariat",
-            content: "Fier de ma conduite accompagné, j’ai commencé a vouloir avoir ma propre voiture. Les 306, je les\n" +
-                " trouvais tellement belles. Intemporelles, avec une gueule agressive, et pas trop moderne pour pouvoir\n" +
-                " apprendre. Dès ma première, je suis tomber amoureux. Premières modifications, premières rencontres\n" +
-                " avec les copains, première leçon de mécaniques. Suite à un accident, j’ai repris une 306 HDI que je\n" +
-                " possède toujours, la famille s’étant agrandie d’un break 1.8 16s très bien équipé que je remonte,\n" +
-                " chose impossible pour moi avant tout ça. Que de souvenirs partagés grâce à ce modèle.",
-            car: "306 2.0 HDI 5p Gris Aster et 306 1.8 16s break Gris Aster",
-            localisation: "Guingamp (22)",
-            facebook: "https://www.facebook.com/club306france/",
-            insta: "https://www.instagram.com/club306officielfrance",
-            mail: "partenariat@club306.fr"
-        },
-        {
-            picture: "/images/clubGachetMickael.jpg",
-            name: "Mikael Gachet",
-            role: "Responsable communication",
-            content: "C’est ma première voiture, j’ai tellement de bons souvenirs avec elle aux quatre coins de la\n" +
-                " France qu’il m’est impossible de m’en séparer. J’ai tout appris avec elle niveau mécanique et elle\n" +
-                " m’a permis de rencontrer des gens qui sont aujourd’hui de vrais amis",
-            car: "306 1.4 ph2. 1 de 1997 Équinoxe",
-            localisation: "Segré (49) ",
-            facebook: "https://www.facebook.com/club306france/",
-            insta: "https://www.instagram.com/club306officielfrance",
-            mail: "communication@club306.fr"
-        },
-        {
-            picture: "/images/clubThiouxEstel.png",
-            name: "Estel Thioux",
-            role: "Adjointe Communication",
-            content: "Je suis issue d’une famille où beaucoup de membres travaillent (ou ont travaillé) dans le milieu\n" +
-                " de l’automobile. J’ai passé mon permis de conduire sur une 306 XRDT. Après avoir longtemps roulé\n" +
-                " au quotidien en 309 GTI, j’ai également eu (entre autres) plusieurs modèles emblématiques de Renault\n" +
-                " avant de revenir à une 306 avec un historique connu. J’ai choisi ce modèle en particulier pour sa\n" +
-                " fiabilité, son côté pratique et sa facilité d’entretien. Je fais également partie d’un club local de véhicules anciens. ",
-            car: "306 TD 1995 – R16 L 1973 – R25 Txi J.O. 1992 – Porsche 944 1983",
-            localisation: "Rosporden (29)",
+            picture: "/images/clubWerleFabreAmandine.jpg",
+            name: "Amandine Werlé-Fabre",
+            role: "Secrétaire/Vice-Président",
+            content: "C’est un très beau break 306, bleu iceland, dans un état exceptionnel mais malheureusement\n" +
+              " disparu dans un accident, qui m’a donné la passion de la 306, et révélé ma passion pour les\n" +
+              " voitures youngtimers.\n" +
+              " Conduire une 306, c’est refuser cette ambiance générale morose et culpabilisante pour tout ce qui\n" +
+              " rend heureux. C’est avoir la simplicité, le charisme, l’essentiel entre les mains, un zeste de l’ADN\n" +
+              " de la mythique 205 qui m’a fait tant rêver à l’adolescence, avec la modernité en plus\n" +
+              " Mon souhait, avec le club 306, est de valoriser cette géniale petite voiture française",
+            car: "306 1.8 16v Grise Aster et 306 Cabriolet Gris",
+            localisation: "Île-de-France (78)",
             facebook: "https://www.facebook.com/club306france/",
             insta: "https://www.instagram.com/club306officielfrance",
             mail: "secretariat@club306.fr"
         },
         {
             picture: "/images/clubBellevziIsciane.jpg",
-            name: "Isciane Bellevzi",
-            role: "Responsable Evenement",
+            name: "Isciane Belleville",
+            role: "Responsable Evénement",
             content: "Ma première voiture, 3 ans avec une 306 XT HDi dont je garde un très bon souvenir, l’aventure à\n" +
-                " continuer avec ma première bv6 en 2015. C’est avec celle-ci que j’ai commencé à vraiment faire des\n" +
-                " rassemblements, à rencontrer des gens formidables, à prendre plaisir à faire des kilomètres dans\n" +
-                " nos belles montagnes… roadtrip, premiers pas sur circuit… beaucoup de bons souvenirs et je compte bien poursuivre l’aventure!",
+              " continuer avec ma première bv6 en 2015. C’est avec celle-ci que j’ai commencé à vraiment faire des\n" +
+              " rassemblements, à rencontrer des gens formidables, à prendre plaisir à faire des kilomètres dans\n" +
+              " nos belles montagnes… roadtrip, premiers pas sur circuit… beaucoup de bons souvenirs et je compte bien poursuivre l’aventure!",
             car: "306 s16 bv6 1998 jaune Vermeer(GTi Suisse) et 306 s16 bv6 noire Onyx",
-            localisation: "Île-de-France (94), Haute-Savoie (74)",
+            localisation: "Île-de-France (77), Haute-Savoie (74)",
             facebook: "https://www.facebook.com/club306france/",
             insta: "https://www.instagram.com/club306officielfrance",
-            mail: "secretariat@club306.fr"
+            mail: "evenements@club306.fr"
         },
         {
-            picture: "/images/clubBesnardPascal.jpg",
-            name: "Pascal Besnard",
-            role: "Webmaster",
-            content: "Passionné de 306 depuis son apparition, j’ai suivi avec entousiasme son evolution et impact en\n" +
-                " France (sur les routes et rallye). En 2004, j’ai acquis une 306 1.9 Td bleu sigma ph 2.1. Puis en 2010,\n" +
-                " je l ai vendu pour m’acheter une 306 HDI que j’ai preparé pendant 3 ans. Enfin, je me suis acheté\n" +
-                " la version XU10J4RS que j’ai restauré à neuf.",
-            car: "306 S16 BV6 1997 Blanc Banquise Premium et 306 S16 BV6 1999 Gris Iceland",
-            localisation: "Seattle (USA), Angers (49), Toulouse (31)",
+            picture: "/images/clubAzaisAntony.png",
+            name: "Antony Azais",
+            role: "Adjoint Evénement",
+            content: "Mon premier achat, mon propre cadeau d’anniversaire, a été une voiture pas trop chère une 306\n" +
+              " 1.9d ph1.1 et très économique pour un jeune de 17 ans. J’ai aujourd’hui bientôt 26 ans et j’ai\n" +
+              " actuellement une 306 HDI comme daily et une 306 S16 bv6 ph1 pour les moments de plaisir et de détente.\n" +
+              " Ce qui me plaît ce sont ses formes carré des années 90 qui font penser à ces petites sœurs comme la 106\n" +
+              " XSI ou rallye ou même encore la 205, la « simplicité » et l’accessibilité aux réparations pour un peux\n" +
+              " expérimenté dans le domaine de la mécanique comme moi.",
+            car: "306 2.0 HDI 5p de 99 et 306 s16 BV6 de 97",
+            localisation: "Brive-la-Gaillarde (19)",
             facebook: "https://www.facebook.com/club306france/",
             insta: "https://www.instagram.com/club306officielfrance",
-            mail: "webmaster@club306.fr"
-        }
+            mail: "evenements@club306.fr"
+        },
+        {
+            picture: "/images/clubFaurieBastien.jpg",
+            name: "Bastien Faurie",
+            role: "Responsable Communication",
+            content: "Fier de ma conduite accompagné, j’ai commencé a vouloir avoir ma propre voiture. Les 306, je les\n" +
+              " trouvais tellement belles. Intemporelles, avec une gueule agressive, et pas trop moderne pour pouvoir\n" +
+              " apprendre. Dès ma première, je suis tomber amoureux. Premières modifications, premières rencontres\n" +
+              " avec les copains, première leçon de mécaniques. Suite à un accident, j’ai repris une 306 HDI que je\n" +
+              " possède toujours, la famille s’étant agrandie d’un break 1.8 16s très bien équipé que je remonte,\n" +
+              " chose impossible pour moi avant tout ça. Que de souvenirs partagés grâce à ce modèle.",
+            car: "306 2.0 HDI 5p Gris Aster et 306 1.8 16s break Gris Aster",
+            localisation: "Guingamp (22)",
+            facebook: "https://www.facebook.com/club306france/",
+            insta: "https://www.instagram.com/club306officielfrance",
+            mail: "communication@club306.fr"
+        },
+        {
+            picture: "/images/clubWissocqBastien.jpg",
+            name: "Bastien Wissocq",
+            role: "Adjoint Communication",
+            content: "Naissant exactement un an jour pour jour après la sortie de la Peugeot 306,\n" +
+              " j’ai un anniversaire commun dévoué d’avance. Famille très Peugeotistes depuis toujours.,\n" +
+              " C’est en 1999 que mon grand-père acheta la 306 et c’est le début d’une belle aventure et,\n" +
+              " de passion des lionnes. Le jour du choix d’une première voiture il ne fallut pas longtemps,\n" +
+              " pour trouver le modèle que je voulais : Une 306 Ph1 D-turbo! Le sort en à décider autrement et,\n" +
+              " se fut une Sedan (306 3 volumes) diesel atmosphérique a 10min de la maison! Auto sortie d’un autre,\n" +
+              " temps avec qui j’ai découvert la passion de l’automobile dans une autre dimension ! La suite c’est,\n" +
+              " un peu la même pour tout le monde L’évolution des forums puis des pages/groupes Facebook d’entraide,\n" +
+              " de passionné a donné naissances à de belles amitiés, de rassemblements magiques dans des régions ,\n" +
+              " superbes, d’aventures et de road trips 306 indescriptibles, de sessions casse hilarantes et surtout,\n" +
+              " de nombreux weekend et nuits de mécanique imprévu mémorable.",
+            car: "306 XSI 2.0 8s 123ch ph1.1 Noir Onyx 01/94;  XS 1.6 90ch Ph1.2 Rouge Vallelunga;  XT 1.8 16s 112ch Ph2.2 Rouge",
+            localisation: "Lille (59)",
+            facebook: "https://www.facebook.com/club306france/",
+            insta: "https://www.instagram.com/club306officielfrance",
+            mail: "communication@club306.fr"
+        },
+
     ];
 
     return (
         <Layout title="l'équipe de Club 306">
             <div className="container my-24 px-6 mx-auto">
                 <section className="mb-32 text-gray-800 text-center">
-
                     <h2 className="text-3xl font-bold mb-32 text-blue-500">Une équipe à votre écoute</h2>
                     <div className="grid gap-x-6 lg:gap-x-12 md:grid-cols-3">
                         {
