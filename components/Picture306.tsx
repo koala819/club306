c7b533f0039f0ded5d6237ce00a7558b79356b44
koala819 <x.genolhac@gmail.com>
2023-01-05 18:12:50 +0100

    carrousel on index

--- components/Picture306.tsx
+++ components/Picture306.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
-import headerBackground from '../public/images/fondHeader.jpg';
+import carroussel_306_Yellow from '../public/images/carroussel_306_Yellow.jpg';
+import carroussel_rassemblement_306 from '../public/images/carroussel_rassemblement_306.jpg';
+import carroussel_two_306_white from '../public/images/carroussel_two_306_white.jpg';
 import { useInView } from 'react-intersection-observer';
 import styles from '../styles/reactIntersectionObserver.module.css';
+import { useState, useEffect } from 'react';
 
 export default function Picture306() {
+  const images = [carroussel_306_Yellow, carroussel_rassemblement_306, carroussel_two_306_white];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const changeImage = () => {
+    setCurrentIndex((currentIndex + 1) % images.length);
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(changeImage, 10000);
+    return () => clearInterval(intervalId);
+  }, [changeImage]);
+
   const IconScrollDown = () => {
     return (
       <div className={styles.mouse_scroll}>
@@ -22,31 +38,22 @@ export default function Picture306() {
     initialInView: true
   });
   const renderContent = () => {
+
+
+
+
     return (
       <div className='relative h-full w-full'>
         <div className='lg:h-screen overflow-hidden sm:h-1/2'>
-          {/*<Suspense fallback={'Waiting Please...'}>*/}
             <Image
               alt='306 car'
-              src={headerBackground}
+              src={images[currentIndex]}
               fill
               sizes='100vw'
               style={{
                 objectFit: 'cover',
                 objectPosition: 'center'
               }} />
-         {/* </Suspense>*/}
-
-          {/* <Image
-            src={headerBackground}
-            alt='306 car'
-            loading='lazy'
-            fill
-            sizes="100vw"
-            style={{
-              objectFit: "cover",
-              objectPosition: "center"
-            }} />*/}
           <div className='absolute inset-x-0 bottom-0'>
             <IconScrollDown />
           </div>
@@ -54,6 +61,7 @@ export default function Picture306() {
       </div>
     );
   };
+
   return (
     <div className={inView ? `${styles.slider} ${styles.slider__zoom}` : `${styles.slider}`} ref={ref}>
       {renderContent()}
