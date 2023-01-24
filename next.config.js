edc5efc3f4db2d7f4cc1ef48cb00b9432154b248
koala819 <x.genolhac@gmail.com>
2023-01-24 21:20:17 +0100

    find how to change url

--- next.config.js
+++ next.config.js
@@ -11,8 +11,7 @@ module.exports = async () => {
     env: {
 		PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
 		PAYPAL_APP_SECRET: process.env.PAYPAL_APP_SECRET,
-		CLIENT_URL: process.env.CLIENT_URL,
-		CLIENT_URL2: process.env.CLIENT_URL2
+		CLIENT_URL: process.env.CLIENT_URL
     }
   };
 };
