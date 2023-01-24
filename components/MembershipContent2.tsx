548c34562b3575ee25791eef34d84846ebb69a12
koala819 <x.genolhac@gmail.com>
2023-01-24 20:51:29 +0100

    nada

--- components/MembershipContent2.tsx
+++ components/MembershipContent2.tsx
@@ -102,7 +102,7 @@ export default function MembershipContent2(nextStep: any) {
                              className='w-3 h-3' viewBox='0 0 24 24'>
                           <path d='M20 6L9 17l-5-5'></path>
                         </svg>
-                      </span>Etre averti des prochains rasso
+                      </span>Etre averti des prochains rassos
               </p>
 
               <PayPalScriptProvider
