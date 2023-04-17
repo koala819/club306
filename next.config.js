3d591cbee74ba3e7a06b0c0e7cb4802be1d6afb3
koala819 <x.genolhac@gmail.com>
2023-04-17 19:15:23 +0200

    fix record in db

--- next.config.js
+++ next.config.js
@@ -3,20 +3,21 @@ module.exports = async () => {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-      domains: ['lh3.googleusercontent.com',
+      domains: [
+        // 'lh3.googleusercontent.com',
         'www.thebigcarshow.com',
         'scontent-cdt1-1.xx.fbcdn.net',
-        'images.unsplash.com']
+        'images.unsplash.com',
+      ],
     },
     env: {
-		PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
-		PAYPAL_APP_SECRET: process.env.PAYPAL_APP_SECRET,
-		CLIENT_URL: process.env.CLIENT_URL,
-		MAIL_USER: process.env.MAIL_USER,
-		MAIL_PWD: process.env.MAIL_PWD,
-		USR_CHECK_DB: process.env.USR_CHECK_DB,
-		USR_CHECK_DB2: process.env.USR_CHECK_DB2
-    }
+      PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
+      PAYPAL_APP_SECRET: process.env.PAYPAL_APP_SECRET,
+      CLIENT_URL: process.env.CLIENT_URL,
+      MAIL_USER: process.env.MAIL_USER,
+      MAIL_PWD: process.env.MAIL_PWD,
+      USR_CHECK_DB: process.env.USR_CHECK_DB,
+      USR_CHECK_DB2: process.env.USR_CHECK_DB2,
+    },
   };
 };
-
