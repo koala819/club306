37d8551fb20ff4ce378e03ae4d148a1d2456d87b
koala819 <x.genolhac@gmail.com>
2023-05-31 08:36:17 +0200

    update code with remove unsuse code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,19 @@
 import Link from 'next/link';
 import { PrismicProvider } from '@prismicio/react';
-// import { PrismicPreview } from '@prismicio/next';
-// import { linkResolver, repositoryName } from '../prismicio';
 import { SessionProvider } from 'next-auth/react';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: any) {
   return (
     <SessionProvider session={pageProps.session}>
       <PrismicProvider
-        // linkResolver={linkResolver}
         internalLinkComponent={({ href, ...props }) => (
-          <LocalizationProvider dateAdapter={AdapterMoment}>
-            <Link href={href}>
-              <a {...props} />
-            </Link>
-          </LocalizationProvider>
+          <Link href={href}>
+            <a {...props} />
+          </Link>
         )}
       >
-        {/* <PrismicPreview repositoryName={repositoryName}> */}
         <Component {...pageProps} />
-        {/* </PrismicPreview> */}
       </PrismicProvider>
     </SessionProvider>
   );
