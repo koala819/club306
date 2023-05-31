583bb36c9b48098d8667caf04cbb5e89910a484e
koala819 <x.genolhac@gmail.com>
2023-05-31 08:36:17 +0200

    update code with remove unsuse code

--- next.config.js
+++ next.config.js
@@ -1,26 +1,28 @@
-module.exports = async () => {
-  return {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-      domains: [
-        'images.prismic.io',
-        'www.thebigcarshow.com',
-        'scontent-cdt1-1.xx.fbcdn.net',
-        'images.unsplash.com',
-        'raw.githubusercontent.com',
-        'scontent-cdg4-2.xx.fbcdn.net',
-        'avatars.githubusercontent.com',
-      ],
-    },
-    env: {
-      PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
-      PAYPAL_APP_SECRET: process.env.PAYPAL_APP_SECRET,
-      CLIENT_URL: process.env.CLIENT_URL,
-      MAIL_USER: process.env.MAIL_USER,
-      MAIL_PWD: process.env.MAIL_PWD,
-      USR_CHECK_DB: process.env.USR_CHECK_DB,
-      USR_CHECK_DB2: process.env.USR_CHECK_DB2,
-    },
-  };
-};
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      'images.prismic.io',
+      'www.thebigcarshow.com',
+      'scontent-cdt1-1.xx.fbcdn.net',
+      'images.unsplash.com',
+      'raw.githubusercontent.com',
+      'scontent-cdg4-2.xx.fbcdn.net',
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+    ],
+  },
+  env: {
+    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
+    PAYPAL_APP_SECRET: process.env.PAYPAL_APP_SECRET,
+    CLIENT_URL: process.env.CLIENT_URL,
+    MAIL_USER: process.env.MAIL_USER,
+    MAIL_PWD: process.env.MAIL_PWD,
+    USR_CHECK_DB: process.env.USR_CHECK_DB,
+    USR_CHECK_DB2: process.env.USR_CHECK_DB2,
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false,
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    // './node_modules/flowbite/**/*.js',
   ],
   theme: {
     fontWeight: {},
     extend: {},
   },
-  plugins: [
-    // require('flowbite/plugin')
-  ],
+  plugins: [],
 };
