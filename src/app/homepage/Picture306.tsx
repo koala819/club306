1904a54793c97f1cf1597ee883e4d2eaebfbedee
koala819 <x.genolhac@gmail.com>
2023-06-13 21:54:04 +0200

    update display first page

--- src/app/homepage/Picture306.tsx
+++ src/app/homepage/Picture306.tsx
@@ -1,13 +1,15 @@
 'use client';
 import { useKeenSlider } from 'keen-slider/react';
 import 'keen-slider/keen-slider.min.css';
-import styles from '../styles/keen.module.css';
+import styles from './keen.module.css';
 import carroussel_306_Yellow from '../../../public/images/carroussel_306_Yellow.jpg';
 import carroussel_rassemblement_306 from '../../../public/images/carroussel_rassemblement_306.jpg';
 import carroussel_rasso_yt from '../../../public/images/carroussel_rassemblement_Young_Timer_2023.jpg';
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import loader from '../../../public/images/loader.svg';
+import stylesInter from './reactIntersectionObserver.module.css';
+import { useInView } from 'react-intersection-observer';
 
 export default function Picture306() {
   const [loaded, setLoaded] = useState<boolean[]>([]);
@@ -37,7 +39,7 @@ export default function Picture306() {
     );
   };
 
-  const [ref] = useKeenSlider<HTMLDivElement>(
+  const [refKeen] = useKeenSlider<HTMLDivElement>(
     {
       animationEnded(s) {
         setCurrentSlide(s.track.details.rel);
@@ -83,33 +85,53 @@ export default function Picture306() {
     setLoaded(new_loaded);
   }, [currentSlide]);
 
-  return (
-    <div ref={ref} className='keen-slider h-full'>
-      {images.map((picture, idx) => (
-        <div
-          key={idx}
-          className={`keen-slider__slide lazy__slide`}
-          style={{ height: '85vh' }}
-        >
-          <div className='flex justify-center items-center w-full h-full bg-blue-300'>
-            <div
-              className={` ${!loaded[idx] ? 'w-1/12 h-1/12' : 'w-full h-full'}`}
-            >
-              <Image
-                alt={picture.alt}
-                height={2000}
-                width={2000}
-                src={loaded[idx] ? picture.src : loader}
-                property='priority'
-                className='object-cover object-center w-full h-full'
-              />
-              <div className='absolute inset-x-0 bottom-0'>
-                <IconScrollDown />
+  const { ref, inView } = useInView({
+    /* Optional options */
+    threshold: 0.4,
+  });
+  const renderContent = () => {
+    return (
+      <div ref={refKeen} className="keen-slider h-full">
+        {images.map((picture, idx) => (
+          <div
+            key={idx}
+            className={`keen-slider__slide lazy__slide`}
+            style={{ height: '85vh' }}
+          >
+            <div className="flex justify-center items-center w-full h-full bg-blue-300">
+              <div
+                className={` ${
+                  !loaded[idx] ? 'w-1/12 h-1/12' : 'w-full h-full'
+                }`}
+              >
+                <Image
+                  alt={picture.alt}
+                  height={2000}
+                  width={2000}
+                  src={loaded[idx] ? picture.src : loader}
+                  property="priority"
+                  className="object-cover object-center w-full h-full"
+                />
+                <div className="absolute inset-x-0 bottom-0">
+                  <IconScrollDown />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+    );
+  };
+  return (
+    <div
+      className={
+        inView
+          ? `${stylesInter.slider} ${stylesInter.slider__zoom}`
+          : `${stylesInter.slider}`
+      }
+      ref={ref}
+    >
+      {renderContent()}
     </div>
   );
 }
--- src/app/partners/Partners.tsx
+++ src/app/partners/Partners.tsx
@@ -1,17 +1,36 @@
 'use client';
 import { Slider } from './Slider';
+import styles from '../homepage/reactIntersectionObserver.module.css';
+import { useInView } from 'react-intersection-observer';
 
 export const Partners = () => {
-  return (
-    <div className='flex items-center justify-center h-full'>
-      <main className='flex flex-col content-center justify-center'>
-        <h1 className='font-bold text-[32px] text-center'>Nos Partenaires</h1>
-        <div className='w-full flex justify-center'>
-          <div className='w-3/4'>
-            <Slider />
+  const { ref, inView } = useInView({
+    /* Optional options */
+    threshold: 0.4,
+  });
+  const renderContent = () => {
+    return (
+      <div className="flex items-center justify-center h-full">
+        <main className="flex flex-col content-center justify-center">
+          <h1 className="font-bold text-[32px] text-center">Nos Partenaires</h1>
+          <div className="w-full flex justify-center">
+            <div className="w-3/4">
+              <Slider />
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
+    );
+  };
+
+  return (
+    <div
+      className={
+        inView ? `${styles.slider} ${styles.slider__zoom}` : `${styles.slider}`
+      }
+      ref={ref}
+    >
+      {renderContent()}
     </div>
   );
 };
