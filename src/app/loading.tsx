617a8de1aef4818e9db4c16b628ce18a446c87d1
koala819 <x.genolhac@gmail.com>
2023-06-16 12:05:46 +0200

    try to improve perf on first page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,15 @@
-export default function Loading() {
+'use client';
+import React from 'react';
+import { Skeleton } from 'antd';
+
+const Loading: React.FC = () => {
   // Or a custom loading skeleton component
   return (
-    <div className="h-screen w-screen bg-blue-300 flex items-center justify-center">
-      <h1 className="text-2x  text-red-500">Chargement de la page</h1>
+    <div className="flex items-center justify-center h-full">
+      <div className="w-1/2">
+        <Skeleton active />
+      </div>
     </div>
   );
-}
+};
+export default Loading;
