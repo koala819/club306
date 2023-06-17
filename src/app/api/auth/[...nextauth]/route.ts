553a23d4c18cd41b3f3415087e119dd02ebe64ac
koala819 <x.genolhac@gmail.com>
2023-06-17 12:09:22 +0200

    fix login in localhost + ADD LOADER

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -25,10 +25,6 @@ const handler = NextAuth({
       },
     }),
   ],
-  session: {
-    strategy: 'jwt',
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
 });
 
 export { handler as GET, handler as POST };
