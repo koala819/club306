bedccdb7ec7f54a13f9d2c3585d635255f3cfa93
koala819 <x.genolhac@gmail.com>
2023-06-19 00:16:14 +0200

    fix error to build

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import { signInGoogle } from '@/lib/signInGoogle';
 import Link from 'next/link';
 import { signIn } from 'next-auth/react';
