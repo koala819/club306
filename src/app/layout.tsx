4c8367a3f2ce5def790066753f3e8a6acef4b77d
koala819 <x.genolhac@gmail.com>
2023-07-04 15:19:24 +0200

    fix to deploy antd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 import { StyleProvider } from '@ant-design/cssinjs';
 import { Footer } from '@/components/Footer';
 import { Navbar } from '@/components/Navbar';
-import { ThemeProvider } from 'next-themes';
 import './globals.css';
 import Providers from '@/components/Providers';
 import { ReactNode } from 'react';
@@ -18,7 +17,6 @@ export default function RootLayout({ children }: LayoutProps) {
         <StyleProvider hashPriority="high">
           <div className=" flex flex-col h-screen">
             <Providers>
-              {/* <NewNavbar /> */}
               <Navbar />
               <main className="flex-1 ">{children}</main>
               <Footer />
