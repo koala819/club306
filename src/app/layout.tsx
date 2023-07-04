632e3d6f037b27488af54f319f41b401acd7e7c7
koala819 <x.genolhac@gmail.com>
2023-07-04 22:22:17 +0200

    fix or not ?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 'use client';
-import { StyleProvider } from '@ant-design/cssinjs';
+// import { StyleProvider } from '@ant-design/cssinjs';
 import { Footer } from '@/components/Footer';
 import { Navbar } from '@/components/Navbar';
-import './globals.css';
+import '@/app/globals.css';
 import Providers from '@/components/Providers';
 import { ReactNode } from 'react';
+import 'antd/dist/reset.css';
 
 interface LayoutProps {
   children: ReactNode;
@@ -14,15 +15,15 @@ export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="fr" className="light">
       <body>
-        <StyleProvider hashPriority="high">
-          <div className=" flex flex-col h-screen">
-            <Providers>
-              <Navbar />
-              <main className="flex-1 ">{children}</main>
-              <Footer />
-            </Providers>
-          </div>
-        </StyleProvider>
+        {/* <StyleProvider hashPriority="high"> */}
+        <div className=" flex flex-col h-screen">
+          <Providers>
+            <Navbar />
+            <main className="flex-1 ">{children}</main>
+            <Footer />
+          </Providers>
+        </div>
+        {/* </StyleProvider> */}
       </body>
     </html>
   );
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,15 +2,63 @@
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import React, { ReactNode } from 'react';
+import { useTheme } from 'next-themes';
+import { ConfigProvider, theme } from 'antd';
 
 interface Props {
   children: ReactNode;
 }
 
+// export function AntdConfigProvider({ children }: Props) {
+//   const { theme: nowTheme } = useTheme();
+
+//   return (
+//     <ConfigProvider
+//       theme={{
+//         algorithm:
+//           nowTheme === 'dark' ? theme.darkAlgorithm : theme.defaultAlgorithm,
+//       }}
+//     >
+//       {children}
+//     </ConfigProvider>
+//   );
+// }
+
 const Providers = (props: Props) => {
+  // const [mounted, setMounted] = useState(false);
+
+  const { theme: nowTheme } = useTheme();
+
+  // // useEffect only runs on the client, so now we can safely show the UI
+  // useEffect(() => {
+  //   setMounted(true);
+  // }, []);
+
+  // if (!mounted) {
+  //   // use your loading page
+  //   return <div className="hidden">{props.children}</div>;
+  // }
+
   return (
-    <ThemeProvider enableSystem attribute="class">
-      <SessionProvider>{props.children}</SessionProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      // disableTransitionOnChange
+    >
+      <SessionProvider>
+        <ConfigProvider
+          theme={{
+            algorithm:
+              nowTheme === 'dark'
+                ? theme.darkAlgorithm
+                : theme.defaultAlgorithm,
+          }}
+        >
+          {props.children}
+        </ConfigProvider>
+        {/* {props.children} */}
+      </SessionProvider>
     </ThemeProvider>
   );
 };
