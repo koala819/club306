f701f352cb7c9af2a2499662b229264814d34f27
koala819 <x.genolhac@gmail.com>
2023-07-06 01:09:03 +0200

    add Alert to invit member to change car infos

--- src/app/homepage/Homepage.tsx
+++ src/app/homepage/Homepage.tsx
@@ -4,10 +4,34 @@ import { BlogSection } from './BlogSection';
 import { Partners } from '../partners/Partners';
 import { Presentation } from './Presentation';
 import { ArticlesProps } from '../types/Components';
+import Alert from '@/components/Alert';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { checkForStartSession } from '@/lib/supabase';
 
 export const Homepage = ({ articles }: ArticlesProps) => {
+  const { data: session } = useSession();
+  const [registeredMember, setRegisteredMember] = useState(false);
+  const [displayBox, setDisplayBox] = useState(true);
+
+  useEffect(() => {
+    if (session?.user !== undefined) {
+      if (Object.keys(session?.user).length !== 0) {
+        checkForStartSession(session).then((response) => {
+          setRegisteredMember(response);
+        });
+      } else {
+        console.log('we have a standard account with login / pwd');
+        setRegisteredMember(true);
+      }
+    }
+  }, [session]);
+
   return (
     <div>
+      {registeredMember && displayBox && (
+        <Alert setDisplayBox={setDisplayBox} />
+      )}
       <div className="hidden md:block">
         <Picture306 />
       </div>
