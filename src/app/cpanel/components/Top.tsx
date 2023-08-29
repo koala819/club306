5e76a9708d48282f456ef50120c29e1ec45b6645
koala819 <x.genolhac@gmail.com>
2023-08-29 11:31:57 +0200

    addon to hide menu on click out

--- src/app/cpanel/components/Top.tsx
+++ src/app/cpanel/components/Top.tsx
@@ -1,16 +1,31 @@
 'use client';
 import { signOut, useSession } from 'next-auth/react';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { AiOutlineLogout } from 'react-icons/ai';
 import Link from 'next/link';
 
 export default function Top() {
   const { data: session, status } = useSession();
   const [isMenuVisible, setMenuVisible] = useState(false);
+  const menuRef = useRef<HTMLDivElement | null>(null);
 
   const toggleMenu = () => {
     setMenuVisible(!isMenuVisible);
   };
+
+  const handleClickOutside = (event: any) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setMenuVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <nav className="p-4 flex border-b-2 border-gray-400 text-blue-600">
@@ -81,7 +96,10 @@ export default function Top() {
         </div>
         {/* Menu Items */}
         {isMenuVisible && (
-          <div className="absolute mt-64 right-4 bg-white border border-gray-300 w-64 shadow-lg text-gray-800">
+          <div
+            ref={menuRef}
+            className="absolute mt-64 right-4 bg-white border border-gray-300 w-64 shadow-lg text-gray-800"
+          >
             <div className="p-2">
               <div className="flex items-center justify-center mb-8 mt-2">
                 <div className="w-1/4">
