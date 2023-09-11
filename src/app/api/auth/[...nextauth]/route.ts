28c0fe5911f92188392617e14088bedbe46eded1
koala819 <x.genolhac@gmail.com>
2023-09-11 20:12:23 +0200

    original cod of nextauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,7 @@ import { SupabaseAdapter } from '@auth/supabase-adapter';
 import { Adapter } from 'next-auth/adapters';
 import type { AuthOptions } from 'next-auth';
 
-export const authOptions: AuthOptions = {
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID || '',
@@ -52,8 +52,6 @@ export const authOptions: AuthOptions = {
   pages: {
     signIn: '/auth/signIn',
   },
-};
-
-const handler = NextAuth(authOptions);
+});
 
 export { handler as GET, handler as POST };
