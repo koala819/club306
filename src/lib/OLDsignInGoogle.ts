bccdc48378782556ed5aa4a3606d618960ae64e6
koala819 <x.genolhac@gmail.com>
2023-09-20 13:02:29 +0200

    remove unused lib

--- src/lib/OLDsignInGoogle.ts
+++
@@ -1,9 +0,0 @@
-import { signIn } from 'next-auth/react';
-
-const signInGoogle = async () => {
-  await signIn('google', {
-    callbackUrl: `${process.env.CLIENT_URL}` || `${process.env.CLIENT_URL2}`,
-  });
-};
-
-export { signInGoogle };
