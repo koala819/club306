24e4ac237ab0503a06c356769157ee0e3082d3cb
koala819 <x.genolhac@gmail.com>
2023-09-29 08:19:35 +0200

    improve Partners from cpanel

--- src/app/cpanel/partners/page.tsx
+++ src/app/cpanel/partners/page.tsx
@@ -1,13 +1,5 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { Partners } from '@/components/Partners';
+import { Partners } from '@/components/cpanel/Partners';
 
 export default async function OurPartners() {
-  const supabase = createServerComponentClient({ cookies });
-
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  return <Partners session={session} />;
+  return <Partners />;
 }
