ac8f65f2e418693884d4047c58eca448583a085d
koala819 <x.genolhac@gmail.com>
2023-12-26 16:41:20 +0100

    improve display memberfinish

--- src/app/(site)/memberfinish/page.tsx
+++ src/app/(site)/memberfinish/page.tsx
@@ -1,11 +1,11 @@
 'use client';
-import WaitSession from '@/components/membership/WaitSession';
+import ClipLoader from 'react-spinners/ClipLoader';
 import MailConfirm from '@/components/membership/MailConfirm';
 import { useEffect, useState } from 'react';
 import connect from '@/lib/helloAsso/connect';
 
 export default function Page() {
-  const [showThankYou, setShowThankYou] = useState<boolean>(false);
+  const [confirmMemberId, setConfirmMemberId] = useState<boolean>(false);
   const [userId, setUserId] = useState<string>('');
 
   useEffect(() => {
@@ -20,7 +20,7 @@ export default function Page() {
           method: 'GET',
         });
         setUserId(result.metadata.userId);
-        setShowThankYou(true);
+        setConfirmMemberId(true);
       } else {
         console.log('error in fetchData', paymentCode, orderId);
       }
@@ -30,12 +30,45 @@ export default function Page() {
   }, []);
 
   return (
-    <div>
-      {showThankYou ? (
-        <MailConfirm userIdFromlocalStorage={userId} />
-      ) : (
-        <WaitSession />
-      )}
+    <div className="flex flex-col items-center justify-center mt-8">
+      <title>Confirmation de votre inscription</title>
+
+      <div className=" w-1/2">
+        <div className="flex w-full items-center justify-center">
+          <div className="w-full">
+            <div className="flex bg-white dark:bg-gray-800 text-gray-700 dark:text-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
+              <picture
+                className="hidden lg:block lg:w-1/2 bg-cover"
+                style={{
+                  backgroundImage:
+                    'url(https://raw.githubusercontent.com/koala819/Unlimitd_front/develop/306.jpg)',
+                  backgroundPositionX: 'left -270px',
+                }}
+              />
+              {confirmMemberId ? (
+                <MailConfirm userIdFromlocalStorage={userId} />
+              ) : (
+                <section className="w-full lg:w-1/2 py-8">
+                  <div className="flex flex-col items-center justify-center space-y-2">
+                    <h2 className="text-2xl font-semibold text-center mb-16">
+                      Mise à jour de votre adhésion
+                    </h2>
+                    <ClipLoader
+                      loading={true}
+                      size={120}
+                      aria-label="Loading Spinner"
+                      data-testid="loader"
+                    />
+                    <p className="pt-16 text-base text-gray-600">
+                      Veuillez patienter
+                    </p>
+                  </div>
+                </section>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
