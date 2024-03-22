5e366844b13ea7c07cea342a831cbc8f40a0c838
koala819 <x.genolhac@gmail.com>
2024-03-22 06:19:16 +0100

    update event page

---
+++ src/components/Event.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import Image from 'next/image'
+
+import { EventsData } from '@/types/models'
+
+import { EventCard } from '@/components/EventCard'
+
+import { getAllEvents } from '@/lib/events'
+
+export default function Event() {
+  const [events, setEvents] = useState<EventsData[]>([])
+
+  useEffect(() => {
+    async function fetchEvents() {
+      const eventResponse = await getAllEvents(new Date().getFullYear())
+      if (eventResponse !== undefined && Array.isArray(eventResponse)) {
+        setEvents(eventResponse)
+      }
+    }
+    fetchEvents()
+  }, [])
+
+  return (
+    <div>
+      <Image
+        src="/images/bandeauEvent.png"
+        width={1920}
+        height={280}
+        alt="Huit Peugeot 306 alignées dans un champ"
+        className="object-cover w-full"
+      />
+
+      <h1 className="mt-20 text-center px-10 xl:mx-32 xl:px-0 xl:text-left">
+        Découvrez les prochains événements
+      </h1>
+      <p className="lg:mx-32 mt-10 mb-20 w-3/4 text-center xl:text-left">
+        Rejoignez-nous pour vivre des moments passionnants, connectez-vous avec
+        d'autres amateurs de voitures et participez à des expériences
+        inoubliables sur la route et au-delà !
+      </p>
+      <div className="mx-4 xl:mx-32">
+        {events
+          .slice()
+          .sort((a, b) => (a.month ?? 0) - (b.month ?? 0))
+          .map((event, index) => {
+            if (!event.title || !event.place || !event.description) {
+              return null
+            }
+
+            return <EventCard key={index} event={event} />
+          })}
+      </div>
+    </div>
+  )
+}
---
+++ src/components/EventCard.tsx
@@ -0,0 +1,122 @@
+'use client'
+
+import { Button, Tooltip } from '@nextui-org/react'
+import { useSession } from 'next-auth/react'
+
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+import { EventsData } from '@/types/models'
+
+import blackLogo from '@/../public/images/logoClub306.png'
+import whiteLogo from '@/../public/images/logoClub306_blanc.png'
+import { useTheme } from '@/context/ThemeContext'
+
+export function EventCard({ event }: { event: EventsData }) {
+  const { data: session } = useSession()
+  const { theme } = useTheme()
+  const router = useRouter()
+
+  const dateToDisplay = formatDate(event.dates, event.month)
+  const imgSrc = event.img || (theme === 'light' ? blackLogo : whiteLogo)
+
+  return (
+    <div className="flex flex-col xl:flex-row border-t-2 min-h-60 border-gray dark:border-text-dark items-center">
+      {/* container img + date */}
+      <div className="w-full xl:w-1/4 text-principal-light dark:text-principal-dark m-8 sm:pt-0">
+        <Image
+          src={imgSrc}
+          alt={event.title}
+          height={256}
+          width={256}
+          className="mx-auto xl:mb-0 xl:mt-0 sm:mb-5 sm:mt-10"
+        />
+
+        <p className="flex justify-center pt-4 text-xl text-center font-semibold">
+          {dateToDisplay}
+        </p>
+      </div>
+      {/* container info */}
+      <div className="w-full xl:w-3/4 p-4 flex-col justify-center">
+        <h2 className="text-center xl:text-left">{event.title}</h2>
+        <p className="text-center xl:text-left text-gray-500">{event.place}</p>
+        <p className="text-center px-20 pt-6 xl:text-left xl:p-0 xl:pt-5">
+          {event.description}
+        </p>
+      </div>
+      {/* container btn */}
+      <div className="flex-col items-center justify-center relative xl:m-5 my-10">
+        {session ? (
+          <Button
+            className="btn-custom"
+            onClick={() => router.push('/eventsRegistration')}
+          >
+            Réserver ma place
+          </Button>
+        ) : (
+          <Tooltip
+            content={
+              <div className="bg-white border border-principal-light p-2 rounded shadow-md dark:hover:bg-[#000015] dark:border-principal-dark m-4">
+                <p className="w-full text-principal-light font-bold p-4 dark:text-principal-dark">
+                  Je suis déjà membre
+                </p>
+                <div className="flex justify-center">
+                  <Button
+                    className="btn-custom"
+                    onClick={() => router.push('/eventsRegistration')}
+                  >
+                    Connexion
+                  </Button>
+                </div>
+
+                <p className="text-principal-light font-bold p-4 dark:text-principal-dark">
+                  Sinon devenez membre
+                </p>
+                <div className="flex justify-center ">
+                  <Button
+                    className="btn-custom"
+                    onClick={() => router.push('/membership')}
+                  >
+                    S'inscrire
+                  </Button>
+                </div>
+              </div>
+            }
+          >
+            <Button className="btn-custom">Réserver ma place</Button>
+          </Tooltip>
+        )}
+      </div>
+    </div>
+  )
+}
+
+function formatDate(date: string, month: number) {
+  if (
+    date.toLowerCase() == 'dates à venir' ||
+    date.toLowerCase() == '(dates à venir)'
+  ) {
+    const months = [
+      'Janvier',
+      'Février',
+      'Mars',
+      'Avril',
+      'Mai',
+      'Juin',
+      'Juillet',
+      'Août',
+      'Septembre',
+      'Octobre',
+      'Novembre',
+      'Décembre',
+    ]
+
+    const monthIndex = month - 1
+    const newMonth = months[monthIndex]
+    const dateToDisplay = `${newMonth} ${date}`
+
+    return dateToDisplay
+  }
+
+  return date
+}
