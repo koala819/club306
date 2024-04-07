b80639fe6f14e82562143a0651222306e91d2acf
koala819 <x.genolhac@gmail.com>
2024-04-07 10:18:26 +0200

    fix code Membership

--- src/components/membership/Membership.tsx
+++ src/components/membership/Membership.tsx
@@ -8,14 +8,11 @@ import { Step } from '@/components/membership/Step'
 export default function Membership() {
   const [currentStep, setStep] = useState<number>(1)
   return (
-    // <>
-    //   <title>Inscription</title>
     <main className="h-full mb-4 pr-4 pl-4">
       <div className="h-full lg:grid lg:grid-cols-12">
         <Step step={currentStep} />
         <Form step={currentStep} setStep={setStep} />
       </div>
     </main>
-    // </>
   )
 }
