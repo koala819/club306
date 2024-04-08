da92794066a7fffd4ab04908a1381f9127f97d9f
koala819 <x.genolhac@gmail.com>
2024-04-08 06:59:33 +0200

    fix reponsive display for age stat

--- src/app/cpanel/admin/stats/page.tsx
+++ src/app/cpanel/admin/stats/page.tsx
@@ -1,34 +1,32 @@
-'use client';
-import { useEffect } from 'react';
-import Car from '@/components/cpanel/stats/Car';
-import Age from '@/components/cpanel/stats/Age';
-import InscriptionByMonth from '@/components/cpanel/stats/InscriptionByMonth';
-import Members from '@/components/cpanel/stats/Members';
+'use client'
+
+import { useEffect } from 'react'
+
+import Age from '@/components/cpanel/stats/Age'
+import Car from '@/components/cpanel/stats/Car'
+import InscriptionByMonth from '@/components/cpanel/stats/InscriptionByMonth'
+import Members from '@/components/cpanel/stats/Members'
 
 export default function Home() {
   useEffect(() => {
     const fetchData = async () => {
       try {
       } catch (error) {
-        console.log('error fetch data', error);
+        console.log('error fetch data', error)
       }
-    };
+    }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   return (
     <div className="flex-grow bg-gray-100 p-4 ">
       <section className="text-gray-600 body-font">
-        <div
-        // className="container px-5 py-24 "
-        >
-          <Members />
-          <Car />
-          <InscriptionByMonth />
-          <Age />
-        </div>
+        <Members />
+        <Car />
+        <InscriptionByMonth />
+        <Age />
       </section>
     </div>
-  );
+  )
 }
